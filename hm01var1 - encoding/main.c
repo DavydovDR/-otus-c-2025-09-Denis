9e31c9d340752c208ebb77f26560f315fe6d2fa4
DavydovDR <mymail06.88@mail.ru>
2025-11-06 21:08:56 +0300

    hw01var1 - added bad exit

--- hm01var1 - encoding/main.c
+++ hm01var1 - encoding/main.c
@@ -26,42 +26,39 @@ int main(int argc, char** argv) {
     FILE *result = fopen(argv[3], "w");
 	if(!result) {
         printf("Failed to open or create %s for writing!\n", argv[3]);
-        fclose(source);
-        return EXIT_FAILURE;
+        goto BAD_EXIT;
     }
 
 	if (strcmp(argv[2], "cp1251") == 0) {
 		if (cp1251_to_utf(source, result) != 0) {
 			printf("Failed to conversion from cp1251 to UTF-8!\n");
-			fclose(source);
-			fclose(result);
-			return EXIT_FAILURE;
+
 		}
     } else if (strcmp(argv[2], "iso-8859-5") == 0) {
 		if (iso8859_5_to_utf(source, result) != 0) {
 			printf("Failed to conversion from iso-8859-5 to UTF-8!\n");
-			fclose(source);
-			fclose(result);
-			return EXIT_FAILURE;
+			goto BAD_EXIT;
 		}
     } else if (strcmp(argv[2], "koi8-r") == 0) {
 		if (koi8r_to_utf(source, result) != 0) {
 			printf("Failed to conversion from koi8-r to UTF-8!\n");
-			fclose(source);
-			fclose(result);
-			return EXIT_FAILURE;
+			goto BAD_EXIT;
 		}
 
     } else {
-		fclose(source);
-		fclose(result);
-		printf("Enter one of this codes as current code: \"cp1251\", \"iso-8859-5\", \"koi8-r\"\n");
-		return EXIT_FAILURE;
+    	printf("Enter one of this codes as current code: \"cp1251\", \"iso-8859-5\", \"koi8-r\"\n");
+    	goto BAD_EXIT;
 	}
 
 	fclose(source);
 	fclose(result);
     return EXIT_SUCCESS;
+
+
+BAD_EXIT:
+	fclose(source);
+	fclose(result);
+	return EXIT_FAILURE;
 }
 
 
